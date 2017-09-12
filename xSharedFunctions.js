98c2e77dc3ef8add04f0ea4643a1e9c4e119696e
Rafal Pieniazek <rpieniazek@schubergphilis.com>
2017-09-12 22:06:26 +0200

    shared functions into class

--- xSharedFunctions.js
+++ xSharedFunctions.js
@@ -1,37 +1,9 @@
-var exports = module.exports = {};
+//https://serverless.com/framework/docs/providers/aws/guide/testing/
 
-var AWS = require('aws-sdk');
 const uuid = require('uuid');
-var CognitoSDK = require('amazon-cognito-identity-js');
 
-
-AWS.CognitoIdentityServiceProvider.AuthenticationDetails = CognitoSDK.AuthenticationDetails;
-AWS.CognitoIdentityServiceProvider.CognitoUserPool = CognitoSDK.CognitoUserPool;
-AWS.CognitoIdentityServiceProvider.CognitoUser = CognitoSDK.CognitoUser;
-AWS.CognitoIdentityServiceProvider.CognitoUserAttribute = CognitoSDK.CognitoUserAttribute;
-
-
-module.exports = {
-    generateErrorResponse: function(respCode, dataErr){
-        
-            var responseCode = (respCode === null) ? 400:respCode ;
-            var responseBody = "";
-        
-            var response = {
-              statusCode: responseCode,
-              body:       responseBody
-            };
-            
-            response.body = {
-                status:  "error",
-                error:   dataErr,
-            };
-            
-            response.body = JSON.stringify(response.body);
-        
-            return response;
-    },
-    generateSuccessResponse: function(respCode, dataSuc){
+class xSharedFunctions {  
+    generateSuccessResponse(respCode, dataSuc){
         
             var responseCode = (respCode === null) ? 400:respCode ;
             var responseBody = "";
@@ -49,16 +21,41 @@ module.exports = {
             response.body = JSON.stringify(response.body);
         
             return response;
-    },    
-    isDef: function(v) {
+    }
+
+    generateErrorResponse(respCode, dataErr){
+        
+            var responseCode = (respCode === null) ? 400:respCode ;
+            var responseBody = "";
+        
+            var response = {
+              statusCode: responseCode,
+              body:       responseBody
+            };
+            
+            response.body = {
+                status:  "error",
+                error:   dataErr,
+            };
+            
+            response.body = JSON.stringify(response.body);
+        
+            return response;
+    }
+
+    isDef(v) {
         return v !== undefined && v !== null;
-    },
-    logmsg: function(component, uniqueId, severity,  message) {
+    }
+
+    logmsg(component, uniqueId, severity,  message) {
         var timestamp = new Date().getTime();
         console.log(`[${component}] [${timestamp}] [${uniqueId}][${severity}] ${message}`);
     }
-  };
+
+
+}
   
+module.exports = xSharedFunctions;
 
 
 
